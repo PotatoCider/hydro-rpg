52d8f6435b915df59d884aeee2f43dee10d1e4c5
gohjoseph <josephgoh7@gmail.com>
2021-01-21 00:42:38 +0800

    Add Flow Volume Interface
    - Added custom plugin which has a function `fetchFlowVolume(chip_id, user_id)` which modifies game variables 15-19
    15: daily flow volume (fv)
    16: weekly fv
    17: monthly fv
    18: yearly fv
    19: total fv
    
    - Added "Flow Volume Fetcher" event at Home (lvl 1) (5, 7)
    just to demonstrate fetching the flow volume
    
    - Added a convienence save file to quickly get into game

---
+++ js/plugins/CUSTOM_FetchFlowVolume.js
@@ -0,0 +1,21 @@
+//=============================================================================
+// FetchFlowVolume.js
+//=============================================================================
+
+/*:
+ * @plugindesc Fetches flow volume from a server
+ * @author Joseph goh
+ *
+ * @help <insert help here>
+ */
+
+async function fetchFlowVolume(chip_id, user_id) {
+	const res = await fetch(`http://aipl.duckdns.org:3000/flow_volume?chip_id=${chip_id}&user_id=${user_id}`)
+	const body = await res.json()
+	console.log(body)
+	$gameVariables.setValue(15, body.daily_flow_volume)
+	$gameVariables.setValue(16, body.weekly_flow_volume)
+	$gameVariables.setValue(17, body.monthly_flow_volume)
+	$gameVariables.setValue(18, body.yearly_flow_volume)
+	$gameVariables.setValue(19, body.total_flow_volume)
+}
