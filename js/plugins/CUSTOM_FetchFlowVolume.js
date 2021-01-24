769f6d09e384fb4e0f1a43b1cc8683e7ed17294d
gohjoseph <josephgoh7@gmail.com>
2021-01-24 15:10:29 +0800

    Add signup & save user config to file
    - Added signup function
    before executing `signupFlowVolume()`, make sure that game variable 13 is initialized with the chip ID. The function would automatically assign a random User ID to you and save it in game variable 14.
    
    - Added save config function to fv_config.json
    - This would store user_id & chip_id game variables even after game reset/deleted saves
    in order to load the config file, you need to `loadFVConfig()` and put a Text/Wait command infront in order to give time to fetch the config.
    `saveFVConfig()` is used only if you want to overwrite the values in the fv_config.json file using the game variables.
    
    - Save file turned off BGM

--- js/plugins/CUSTOM_FetchFlowVolume.js
+++ js/plugins/CUSTOM_FetchFlowVolume.js
@@ -4,19 +4,61 @@
 
 /*:
  * @plugindesc Fetches flow volume from a server
- * @author Joseph goh
+ * @author Joseph Goh
  *
  * @help <insert help here>
  */
 
-async function fetchFlowVolume(chip_id, user_id) {
+console.log('fetch flow volume startup')
+
+async function saveFVConfig() {
+	const chip_id = $gameVariables.value(13)
+	const user_id = $gameVariables.value(14)
+	const writeFile = require('util').promisify(require('fs').writeFile)
+	
+	const json = { chip_id, user_id }
+	await writeFile('fv_config.json', JSON.stringify(json))
+}
+
+async function loadFVConfig() {
+	const readFile = require('util').promisify(require('fs').readFile)
+	const json = await readFile('fv_config.json', { encoding: 'utf-8' })
+	const { chip_id, user_id } = JSON.parse(json)
+	$gameVariables.setValue(13, chip_id)
+	$gameVariables.setValue(14, user_id)
+	return { chip_id, user_id }
+}
+
+async function fetchFlowVolume() {
+	if (!$gameVariables.value(13)) // if not initialized
+		await loadFVConfig()
+	const chip_id = $gameVariables.value(13)
+	const user_id = $gameVariables.value(14)
 	const res = await fetch(`http://aipl.duckdns.org:3000/flow_volume?chip_id=${chip_id}&user_id=${user_id}`)
+	if (res.status !== 200)
+		return console.error(res.text())
 	const body = await res.json()
-	console.log(body)
 	$gameVariables.setValue(15, body.daily_flow_volume)
 	$gameVariables.setValue(16, body.weekly_flow_volume)
 	$gameVariables.setValue(17, body.monthly_flow_volume)
 	$gameVariables.setValue(18, body.yearly_flow_volume)
 	$gameVariables.setValue(19, body.total_flow_volume)
 	$gameVariables.setValue(20, body.last_flow_volume)
+}
+
+// Ensure that Game Variable 13 is set to the user entered chip_id
+// Game Variable 14 will be updated with the Chip ID.
+async function signupFlowVolume() {
+	const chip_id = $gameVariables.value(13)
+	if (chip_id == null || chip_id === '' || chip_id <= 0 || chip_id >= 2 ** 32)
+		return console.error('Invalid Chip ID.')
+	
+	const res = await fetch(`http://aipl.duckdns.org:3000/signup`, { method: 'POST', body: JSON.stringify({ chip_id }) })
+	if (res.status !== 200)
+		return console.error(res.text())
+	const { user_id } = await res.json()
+
+	$gameVariables.setValue(13, chip_id)
+	$gameVariables.setValue(14, user_id)
+	await saveFVConfig()
 }
