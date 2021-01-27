44ad629f60a0f61ea360dee52ee66f82c59f7a64
gohjoseph <josephgoh7@gmail.com>
2021-01-28 00:19:19 +0800

    Shifted FV Logic into Common Events
    - Shifted most flow volume logic into common events. Modification of game variables shifted into common events instead of plugin.
    
    - Added error checking for FV scripts
    
    - Added last_timestamp

--- js/plugins/CUSTOM_FetchFlowVolume.js
+++ js/plugins/CUSTOM_FetchFlowVolume.js
@@ -11,9 +11,7 @@
 
 console.log('fetch flow volume startup')
 
-async function saveFVConfig() {
-	const chip_id = $gameVariables.value(13)
-	const user_id = $gameVariables.value(14)
+async function saveFVConfig(chip_id = $gameVariables.value(13), user_id = $gameVariables.value(14)) {
 	const writeFile = require('util').promisify(require('fs').writeFile)
 	
 	const json = { chip_id, user_id }
@@ -24,41 +22,70 @@ async function loadFVConfig() {
 	const readFile = require('util').promisify(require('fs').readFile)
 	const json = await readFile('fv_config.json', { encoding: 'utf-8' })
 	const { chip_id, user_id } = JSON.parse(json)
-	$gameVariables.setValue(13, chip_id)
-	$gameVariables.setValue(14, user_id)
+
+	// $gameVariables.setValue(13, chip_id)
+	// $gameVariables.setValue(14, user_id)
+
 	return { chip_id, user_id }
 }
 
-async function fetchFlowVolume() {
-	if (!$gameVariables.value(13)) // if not initialized
-		await loadFVConfig()
-	const chip_id = $gameVariables.value(13)
-	const user_id = $gameVariables.value(14)
+async function fetchFlowVolume(chip_id = $gameVariables.value(13), user_id = $gameVariables.value(14)) {
+
 	const res = await fetch(`http://aipl.duckdns.org:3000/flow_volume?chip_id=${chip_id}&user_id=${user_id}`)
-	if (res.status !== 200)
-		return console.error(res.text())
+	if (res.status !== 200) {
+		const err = await res.text()
+		console.error(err)
+		$gameVariables.setValue(12, err)
+		return {}
+	}
+		
 	const body = await res.json()
-	$gameVariables.setValue(15, body.daily_flow_volume)
-	$gameVariables.setValue(16, body.weekly_flow_volume)
-	$gameVariables.setValue(17, body.monthly_flow_volume)
-	$gameVariables.setValue(18, body.yearly_flow_volume)
-	$gameVariables.setValue(19, body.total_flow_volume)
-	$gameVariables.setValue(20, body.last_flow_volume)
+
+	// $gameVariables.setValue(12, 0)
+	// $gameVariables.setValue(15, body.daily_flow_volume)
+	// $gameVariables.setValue(16, body.weekly_flow_volume)
+	// $gameVariables.setValue(17, body.monthly_flow_volume)
+	// $gameVariables.setValue(18, body.yearly_flow_volume)
+	// $gameVariables.setValue(19, body.total_flow_volume)
+	// $gameVariables.setValue(20, body.last_flow_volume)
+	return body
 }
 
 // Ensure that Game Variable 13 is set to the user entered chip_id
 // Game Variable 14 will be updated with the Chip ID.
-async function signupFlowVolume() {
-	const chip_id = $gameVariables.value(13)
-	if (chip_id == null || chip_id === '' || chip_id <= 0 || chip_id >= 2 ** 32)
-		return console.error('Invalid Chip ID.')
-	
-	const res = await fetch(`http://aipl.duckdns.org:3000/signup`, { method: 'POST', body: JSON.stringify({ chip_id }) })
-	if (res.status !== 200)
-		return console.error(res.text())
-	const { user_id } = await res.json()
+async function signupFlowVolume(chip_id = $gameVariables.value(13)) {
+	if (isNaN(chip_id) || chip_id <= 0 || chip_id >= 2 ** 32) {
+		$gameVariables.setValue(12, 'Invalid Chip ID')
+		return {}
+	}
 
-	$gameVariables.setValue(13, chip_id)
-	$gameVariables.setValue(14, user_id)
-	await saveFVConfig()
+	const res = await fetch(`http://aipl.duckdns.org:3000/signup`, { 
+		method: 'POST', 
+		headers: { 'Content-Type': 'application/json' }, 
+		body: JSON.stringify({ chip_id })
+	})
+	if (res.status !== 200) {
+		const err = await res.text()
+		console.error(err)
+		$gameVariables.setValue(12, err)
+		return {}
+	}
+	const body = await res.json()
+	
+	// $gameVariables.setValue(12, 0)
+	// $gameVariables.setValue(13, chip_id)
+	// $gameVariables.setValue(14, user_id)
+
+	// await saveFVConfig()
+	return body
+}
+
+async function mockUpdateFlowVolume(flow_volume = 0, chip_id = $gameVariables.value(13), user_id = $gameVariables.value(14)) {
+	const payload = { chip_id, user_id, flow_volume }
+	console.log('payload: ', payload)
+	return fetch(`http://aipl.duckdns.org:3000/flow_volume`, {
+		method: 'POST', 
+		headers: { 'Content-Type': 'application/json' }, 
+		body: JSON.stringify(payload)
+	})
 }
